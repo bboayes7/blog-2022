68eb26c61236545b08266b8a3221fa6d65d9ac84
Barry Boayes <bboayes@gmail.com>
2022-09-18 14:51:58 -0700

    projects crud done

--- client/src/components/pages/Admin/About/EditAbout.tsx
+++ client/src/components/pages/Admin/About/EditAbout.tsx
@@ -3,6 +3,7 @@ import EditProjects from "./Projects/EditProjects"
 import { useState } from 'react'
 const EditAbout = () => {
   const [toggleSkills, setToggleSkills] = useState(false)
+  const [toggleProjects, setToggleProjects] = useState(false)
   
 
   return (
@@ -10,8 +11,9 @@ const EditAbout = () => {
       <h1>Edit About Section</h1>
       <button onClick={() => setToggleSkills(!toggleSkills)}>{toggleSkills ? 'Edit Skills' : 'Hide Edit Skills' }</button>
       {toggleSkills && <EditSkills /> }
-      <p>projects</p>
-      <EditProjects />
+      
+      <button onClick={() => setToggleProjects(!toggleProjects)}>{toggleProjects ? 'Edit Projects' : 'Hide Projects' }</button>
+      {toggleProjects && <EditProjects />}
       <p>experience</p>
       {/* <EditExperience /> */}
     </div>
